a081901b93c690ee7666460e3a099bfcc7f3c58a
ailenperalta <ailen.peralta@estudiantes.unahur.edu.ar>
2026-06-11 17:39:28 -0300

    Versión final

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,79 +1,19 @@
-import Form from "react-bootstrap/Form"
+import Form from "react-bootstrap/Form";
+import { BsSearch } from "react-icons/bs";
+import "./SearchBar.css";
 
-function SearchBar({search, setSearch}) {
+function SearchBar({ search, setSearch }) {
   return (
-    <Form.Control
-      type="text"
-      value={search}
-      placeholder="Buscar por nombre"
-      onChange={(e) => setSearch(e.target.value)}
-    />
+    <div className="catalog-search">
+      <BsSearch className="search-icon" />
+      <Form.Control
+        type="text"
+        value={search}
+        placeholder="Buscar por nombre"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+    </div>
   )
 }
 
 export default SearchBar;
-
-
-// import SearchBar from "../../components/SearchBar/SearchBar";
-//
-// import { useState } from "react";
-//
-// import { products } from "../../data/products"
-//
-// import ProductCard from "../../components/ProductCard/ProductCard";
-// import { Container, Row, Col } from "react-bootstrap";
-//
-
-
-
-//USAR productosFiltrados DE Filters.jsx Y SACAR LO QUE NO SE VA A USAR
-    //   {productosFiltrados.map((producto) => (
-    //     <Card key={producto.id}>
-    //       <Card.Body>
-    //         <Card.Title>{producto.nombre}</Card.Title>
-    //         <Card.Text>${producto.precio}</Card.Text>
-    //       </Card.Body>
-    //     </Card>
-    //   ))}
-    //
-//   esto no va
-    //   {productosFiltrados.map(product => (
-    //     <div key={product.id}>
-    //       {product.nombre}
-    //     </div>
-    //   ))}
-//
-//
-//esto si va
-      //  <Container className='page-container'>
-      //   <SearchBar search={search}
-      //     setSearch={setSearch}
-      //   />
-      // </Container>
-
-// esto va para que vayan apareciendo las coincidencias, empieza vacio, de la otra forma empieza todo el catalogo y quedan las coincidencias
-  // const productosFiltrados =
-  //   search.trim() === ""
-  //     ? []
-  //     : products.filter(product =>
-  //       product.nombre
-  //         .toLowerCase()
-  //         .includes(search.toLowerCase())
-  //     );
-  //
-
-// esto si es en una pagina aparte, si no se usan las cards en products
-    // <>
-    //   <Container className="page-container">
-    //     <SearchBar search={search} setSearch={setSearch} />
-    //     <Row>
-    //       {productosFiltrados.map((producto) => (
-    //         <Col lg={4} md={4} sm={4} key={producto.id}>
-    //           <ProductCard producto={producto} />
-    //         </Col>
-    //       ))}
-    //     </Row>
-    //
-    //   </Container>
-    //
-    // </>
--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,48 +1,48 @@
 import { Link, useParams } from 'react-router';
-import {products} from '../../data/products';
+import { products } from '../../data/products';
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import './ProductDetail.css'
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 
 function ProductDetail() {
-   const {id} = useParams();
-   const {agregarProducto} = useContext(CartContext)
-   const product = products.find((p) => p.id == id);
-   return(
-        <div className = 'contenedorCard'>
-        <Card className="mb-3 estiloDetalle">
-            <Row className = 'g-0'>
-                <Col md= {4}>
-                <Card.Img className= 'estiloImagen' src={product.imagen}></Card.Img>
-                </Col>
-                <Col md={8}>
-                    <Card.Body>
-                       <Card.Title style={{fontSize:'2rem',textAlign:'center'}}>{product.nombre} - {product.autor}</Card.Title>
-                       <Card.Text className='precioDetalles price'> ${product.precio}</Card.Text>
-                    
-                    {product.stock > 0 ?(
-                        <Container className="d-flex gap-3 justify-content-center contendorBotones" >
-                        <Button className= "estiloBotones btn btn-primary-custom" onClick={() => agregarProducto(product)} > Agregar Al Carrito </Button>
-                        <Button className="btn btn-secondary-custom estiloBotones" as={Link} to={`/products`}>Volver</Button>
-                        </Container>
-                    )
-                    :
-                    (   
-                        <Container className="d-flex gap-3 justify-content-center">
-                            <Button className= "buttonSinStock estiloBotones"> Sin Stock </Button>
-                            <Button className="btn btn-secondary-custom estiloBotones" as={Link} to={`/products`}>Volver</Button>
-                        </Container>
-                    )}
-                       <div className='contenedorSinopsis'>
-                        <h3>Sinopsis</h3>
-                        <p className='productoDescripcion'>{product.descripcion}</p>
-                        </div>
-                    </Card.Body>
-                </Col>
-            </Row>
-        </Card>
-    </div>
+    const { id } = useParams();
+    const { agregarProducto } = useContext(CartContext)
+    const product = products.find((p) => p.id == id);
+    return (
+        <div className='contenedorCard'>
+            <Card className="mb-3 estiloDetalle">
+                <Row className='g-0'>
+                    <Col md={4}>
+                        <Card.Img className='estiloImagen' src={product.imagen}></Card.Img>
+                    </Col>
+                    <Col md={8}>
+                        <Card.Body>
+                            <Card.Title style={{ fontSize: '2rem', textAlign: 'center' }}>{product.nombre} - {product.autor}</Card.Title>
+                            <Card.Text className='precioDetalles price'> ${product.precio}</Card.Text>
+
+                            {product.stock > 0 ? (
+                                <Container className="d-flex gap-3 justify-content-center contendorBotones" >
+                                    <Button className="estiloBotones btn btn-primary-custom" onClick={() => agregarProducto(product)} > Agregar Al Carrito </Button>
+                                    <Button className="btn btn-secondary-custom estiloBotones" as={Link} to={`/products`}>Volver</Button>
+                                </Container>
+                            )
+                                :
+                                (
+                                    <Container className="d-flex gap-3 justify-content-center">
+                                        <Button className="buttonSinStock estiloBotones"> Sin Stock </Button>
+                                        <Button className="btn btn-secondary-custom estiloBotones" as={Link} to={`/products`}>Volver</Button>
+                                    </Container>
+                                )}
+                            <div className='contenedorSinopsis'>
+                                <h3>Sinopsis</h3>
+                                <p className='productoDescripcion'>{product.descripcion}</p>
+                            </div>
+                        </Card.Body>
+                    </Col>
+                </Row>
+            </Card>
+        </div>
     );
 }
 export default ProductDetail;
