d87af06a0b19a4625090dd50b3586c9e0b3b957a
ailenperalta <ailen.peralta@estudiantes.unahur.edu.ar>
2026-06-11 10:35:14 -0300

    Checkout form, searchbar, filters

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,8 +1,18 @@
 import Form from "react-bootstrap/Form"
+import "./Filters.css"
 
-function Filters(categories, category, setCategory, stockMin, setStockMin, priceMin, setPriceMin, priceMax, setPriceMax) {
+function Filters({
+  categories, 
+  category, 
+  setCategory, 
+  stockMin, 
+  setStockMin, 
+  priceMin, 
+  setPriceMin, 
+  priceMax, 
+  setPriceMax}) {
   return (
-    <Form.Group>
+    <Form.Group className="custom-number-inputs">
       <Form.Label> Categoria </Form.Label>
       <Form.Select
         value={category}
@@ -22,33 +32,89 @@ function Filters(categories, category, setCategory, stockMin, setStockMin, price
         placeholder="0"
         min={0}
         value={stockMin}
-        onChange={(e) => setStockMin(Number(e.target.value))}
+        onChange={(e) => setStockMin(e.target.value)}
       />
-      <Form.Label> Precio </Form.Label>
-      <Form.Label> Minimo</Form.Label>
+      <Form.Label> Precio Minimo </Form.Label>
       <Form.Control
         type="number"
         placeholder="0"
         min={0}
         value={priceMin}
-        onChange={(e) => setPriceMin(Number(e.target.value))}
+        onChange={(e) => setPriceMin(e.target.value)}
       />
-      <Form.Label> Maximo</Form.Label>
+      <Form.Label> Precio Maximo</Form.Label>
       <Form.Control
         type="number"
         placeholder="0"
         min={0}
         value={priceMax}
-        onChange={(e) => setPriceMax(Number(e.target.value))}
+        onChange={(e) => setPriceMax(e.target.value)}
       />
     </Form.Group>
   )
 }
 
+//
+// import Filters from '../../components/Filters/Filters';
+//
+// import { useState } from "react";
 //const categories = [
 //  ...new Set(products.map(p => p.categoria))
 //  ];
-//]
+// /  const [category, setCategory] = useState("");
+//
+//   const [stockMin, setStockMin] = useState("");
+//
+//   const [priceMin, setPriceMin] = useState("");
+//
+//   const [priceMax, setPriceMax] = useState("");
+//
+//   const categories = [
+//     ...new Set(products.map(p => p.categoria))
+//   ];
+// /]
+//
+// combina busqueda y filtro
+// //  const productosFiltrados = products.filter((producto) => {
+//
+//     const coincideBusqueda =
+//       producto.nombre
+//         .toLowerCase()
+//         .includes(search.toLowerCase());
+//
+//     const coincideCategoria =
+//       category === "" ||
+//       producto.categoria === category;
+//
+//     const coincideStock =
+//       stockMin === "" ||
+//       producto.stock >= Number(stockMin);
+//
+//     const coincidePrecio =
+//       (priceMin === "" ||producto.precio >= Number(priceMin)) &&
+//       (priceMax === "" ||producto.precio <= Number(priceMax));
+//
+//     return (
+//       coincideBusqueda &&
+//       coincideCategoria &&
+//       coincideStock &&
+//       coincidePrecio
+//     );
+//   });
+//      <Container className='page-container'>
+      //   <Filters
+      //     categories={categories}
+      //     category={category}
+      //     setCategory={setCategory}
+      //     stockMin={stockMin}
+      //     setStockMin={setStockMin}
+      //     priceMin={priceMin}
+      //     setPriceMin={setPriceMin}
+      //     priceMax={priceMax}
+      //     setPriceMax={setPriceMax} />
+      // </Container>
+      //
+//
 //esto iria donde se usa
 
 export default Filters;
